751cb4e737378f9088d8c9998995dca4fbeef1f0
Chris Bubel <christopher.s.bubel@gmail.com>
2018-03-30 17:21:56 -0400

    Added hash navigation

--- dist/templates.js
+++ dist/templates.js
@@ -1,14 +1,39 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['index'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+templates['index'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = container.invokePartial(partials["partials/header"],depth0,{"name":"partials/header","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + " "
-    + ((stack1 = container.invokePartial(partials["partials/main"],depth0,{"name":"partials/main","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + " "
-    + ((stack1 = container.invokePartial(partials["partials/footer"],depth0,{"name":"partials/footer","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+  return ((stack1 = container.invokePartial(partials["partials/about"],depth0,{"name":"partials/about","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+},"3":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? depth0.page : depth0)) != null ? stack1.isContact : stack1),{"name":"if","hash":{},"fn":container.program(4, data, 0),"inverse":container.program(6, data, 0),"data":data})) != null ? stack1 : "");
+},"4":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = container.invokePartial(partials["partials/contact"],depth0,{"name":"partials/contact","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+},"6":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = container.invokePartial(partials["partials/home"],depth0,{"name":"partials/home","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "  ";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<div>\n"
+    + ((stack1 = container.invokePartial(partials["partials/header"],depth0,{"name":"partials/header","data":data,"indent":"  ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "</div>\n\n<div id=\"main\" class=\"container\">\n"
+    + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? depth0.page : depth0)) != null ? stack1.isAbout : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
+    + "</div>\n\n<div>\n"
+    + ((stack1 = container.invokePartial(partials["partials/footer"],depth0,{"name":"partials/footer","data":data,"indent":"  ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "</div>";
 },"usePartial":true,"useData":true});
+templates['partials/about'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<h1>About</h1>\n\n<p class=\"lead\">\n  This is the about page.\n</p>";
+},"useData":true});
+templates['partials/contact'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<h1>Contact</h1>";
+},"useData":true});
 templates['partials/footer'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
@@ -17,10 +42,12 @@ templates['partials/footer'] = template({"compiler":[7,">= 4.0.0"],"main":functi
     + "</small>\n</div>";
 },"useData":true});
 templates['partials/header'] = template({"1":function(container,depth0,helpers,partials,data) {
-    var helper;
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "      <li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"#\">"
-    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"name","hash":{},"data":data}) : helper)))
+  return "      <li class=\"nav-item\">\n        <a class=\"nav-link\" href=\""
+    + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
+    + "\">"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "</a>\n      </li>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
@@ -31,11 +58,11 @@ templates['partials/header'] = template({"1":function(container,depth0,helpers,p
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? depth0.header : depth0)) != null ? stack1.links : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "    </ul>\n  </div>\n</nav>";
 },"useData":true});
-templates['partials/main'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+templates['partials/home'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div id=\"main\" class=\"container\">\n  <h1>Template body</h1>\n\n  <p class=\"lead\">\n    "
+  return "<h1>Template body</h1>\n\n<p class=\"lead\">\n  "
     + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.main : depth0)) != null ? stack1.body : stack1), depth0))
-    + "\n  </p>\n</div>";
+    + "\n</p>";
 },"useData":true});
 })();
--- js/context.js
+++ js/context.js
@@ -1,10 +1,17 @@
+let hash = "";
+
 const context = {
+  page: {
+    isHome: false,
+    isAbout: false,
+    isContact: false
+  },
   header: {
     title: "Handlebars Sandbox",
     links: [
-      { name: "Home" },
-      { name: "About" },
-      { name: "Contact" }
+      { name: "Home", url: "#" },
+      { name: "About", url: "#about" },
+      { name: "Contact", url: "#contact" }
     ]
   },
   main: {
@@ -14,3 +21,15 @@ const context = {
     text: "This is the footer"
   }
 };
+
+function setPage(hash) {
+  hash = hash;
+
+  context.page.isHome = hash === "" || hash === "#";
+  context.page.isAbout = hash === "#about";
+  context.page.isContact = hash === "#contact";
+}
+
+Handlebars.registerHelper("is", (arg1, arg2, options) => {
+  return arg1 === arg2;
+});
